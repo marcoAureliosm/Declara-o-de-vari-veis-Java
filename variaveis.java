19972bd02a6f76a69a1908d830f3b0f1736d203d
marcoAureliosm <49442587+marcoAureliosm@users.noreply.github.com>
2019-04-11 18:07:51 -0300

    Declaração variaveis

---
+++ variaveis.java
@@ -0,0 +1,26 @@
+package com.marcoaurelio.cursojava.aula10;
+
+public class variaveis {
+
+	public static void main(String[] args) {
+		//Convenção java
+		int idade=21;
+		String nome="Marco Aurelio"; 
+		String nomeDoCachorro="Cruel";
+		float altura= (float) 1.75; 
+		//Aceito, mas não utilizado
+		int _idade;
+		int $idade;
+		//não é convenção java
+		String nome_do_meu_cachorro;
+		String NomeDoMeuCachorro;
+		String nomeDoMeucachorro;
+		// má pratica
+		int a=10;
+		String b="marco";
+		
+		
+
+	}
+
+}
